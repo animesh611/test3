fd918743ff870c1b613ab6bdc1b45d9aa5857b7a
ANIMESH GHOSH <88232647+animesh611@users.noreply.github.com>
2023-04-14 11:21:20 +0530

    Update AppTest.java
    hgsjsdkjhsgddhsg

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,13 +4,13 @@ package org.example;
 //import junit.framework.TestCase;
 //import junit.framework.TestSuite;
 
-import junit.framework.TestCase;
+// import junit.framework.TestCase;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest 
+//     extends TestCase
+// {
 
-}
+// }
